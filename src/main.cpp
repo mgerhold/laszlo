4721c21c6400a65f963987f23d0f7bda3abcb443
Michael Gerhold <michael.gerhold@web.de>
2023-11-08 22:02:20 +0100

    enabled support for Emscripten

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,16 @@
 }
 
 int main(int argc, const char** argv) try {
+#ifdef EMSCRIPTEN
+    auto filename = std::string_view{ "programs/test.las" };
+#else
     assert(argc >= 1);
     if (argc != 2) {
         std::cerr << std::format("error: no input file\nusage: {} <INPUT_FILENAME>\n", argv[0]);
         return EXIT_FAILURE;
     }
     auto filename = std::string_view{ argv[1] };
+#endif
     auto const source = read_file(filename);
     auto const tokens = Tokens::tokenize(filename, source);
     auto const ast = parse(tokens);
