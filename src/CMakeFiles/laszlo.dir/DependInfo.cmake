f6b14c47d521d169d91bbe5d6df7f06abe2a8d29
mgerhold <mgerhold@users.noreply.github.com>
2023-11-09 20:58:58 +0000

    deploy: 99202a5bb9d757fb8fc68787ba6ac142d55a9bb7

---
+++ src/CMakeFiles/laszlo.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/laszlo/laszlo/src/lexer.cpp" "src/CMakeFiles/laszlo.dir/lexer.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/lexer.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/main.cpp" "src/CMakeFiles/laszlo.dir/main.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/main.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/parser.cpp" "src/CMakeFiles/laszlo.dir/parser.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/parser.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/values/array.cpp" "src/CMakeFiles/laszlo.dir/values/array.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/values/array.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/values/function.cpp" "src/CMakeFiles/laszlo.dir/values/function.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/values/function.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/values/integer.cpp" "src/CMakeFiles/laszlo.dir/values/integer.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/values/integer.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/values/string.cpp" "src/CMakeFiles/laszlo.dir/values/string.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/values/string.cpp.o.d"
+  "/home/runner/work/laszlo/laszlo/src/values/value.cpp" "src/CMakeFiles/laszlo.dir/values/value.cpp.o" "gcc" "src/CMakeFiles/laszlo.dir/values/value.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/laszlo.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laszlo.dir/lexer.cpp.o"
+  "CMakeFiles/laszlo.dir/lexer.cpp.o.d"
+  "CMakeFiles/laszlo.dir/main.cpp.o"
+  "CMakeFiles/laszlo.dir/main.cpp.o.d"
+  "CMakeFiles/laszlo.dir/parser.cpp.o"
+  "CMakeFiles/laszlo.dir/parser.cpp.o.d"
+  "CMakeFiles/laszlo.dir/values/array.cpp.o"
+  "CMakeFiles/laszlo.dir/values/array.cpp.o.d"
+  "CMakeFiles/laszlo.dir/values/function.cpp.o"
+  "CMakeFiles/laszlo.dir/values/function.cpp.o.d"
+  "CMakeFiles/laszlo.dir/values/integer.cpp.o"
+  "CMakeFiles/laszlo.dir/values/integer.cpp.o.d"
+  "CMakeFiles/laszlo.dir/values/string.cpp.o"
+  "CMakeFiles/laszlo.dir/values/string.cpp.o.d"
+  "CMakeFiles/laszlo.dir/values/value.cpp.o"
+  "CMakeFiles/laszlo.dir/values/value.cpp.o.d"
+  "laszlo.html"
+  "laszlo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laszlo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
