fd2da1ef6f96ff57a8e711c806962ce6d256abd3
Michael Gerhold <michael.gerhold@web.de>
2023-11-16 15:38:28 +0100

    reformatting

--- src/statements/struct_definition.hpp
+++ src/statements/struct_definition.hpp
@@ -23,11 +23,7 @@ namespace statements {
         [[nodiscard]] std::string to_string() const {
             auto result = std::format("struct {}(", m_name.lexeme());
             for (auto i = std::size_t{ 0 }; i < m_members.size(); ++i) {
-                result += std::format(
-                        "{}: {}",
-                        m_members.at(i).m_name.lexeme(),
-                        m_members.at(i).m_type->to_string()
-                );
+                result += std::format("{}: {}", m_members.at(i).m_name.lexeme(), m_members.at(i).m_type->to_string());
                 if (i < m_members.size() - 1) {
                     result += ", ";
                 }
